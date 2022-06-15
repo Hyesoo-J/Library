d92f0371813039d410f6b7ff1bd4f0e9d191071e
Hyesoo <shellingphod@gmail.com>
2022-06-15 18:34:06 +0200

    -

---
+++ app/src/main/java/de/buw/se4de/DBConnection.java
@@ -0,0 +1,121 @@
+package de.buw.se4de;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.opencsv.CSVWriter;
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+
+public class DBConnection {
+
+
+    public static List<List<String>> booksDBList = new ArrayList<List<String>>();
+    public static String[][] booksDB;
+
+    protected static String path = Paths.get("src/main/resources/booksDB.csv").getParent() + "\\" + "booksDB.csv";
+
+
+    public static void readBooksDB() {
+        String line = "";
+
+
+        try {
+            BufferedReader buffer = new BufferedReader(new FileReader(path));
+
+            while ((line = buffer.readLine()) != null) {
+                String[] data1D = line.split(",");
+                booksDBList.add(Arrays.asList(data1D));
+               // System.out.println(data1D.toString());
+            }
+        } catch (FileNotFoundException var5) {
+            throw new RuntimeException(var5);
+        } catch (IOException var6) {
+            throw new RuntimeException(var6);
+        }
+
+        booksDB = booksDBList.stream().map((l) -> {
+            return l.stream().toArray((x$0) -> {
+                return new String[x$0];
+            });
+        }).toArray((x$0) -> {
+            return new String[x$0][];
+        });
+    }
+
+    public static void setBook(String bookID, String bookTitle, String author, String price, String publisher){
+
+
+        if (getIndex(bookID) != -1){System.out.println("The book is already registered!");}
+        else {
+            var copy = new String[booksDB.length + 1][booksDB[0].length + 1];
+            for (int i = 0; i < booksDB.length; i++)
+                for (int j = 0; j < booksDB[i].length; j++)
+                    copy[i][j] = booksDB[i][j];
+            for (int i = 0; i < booksDB[0].length; i++)
+                copy[booksDB.length][i] = "null";
+
+            copy[booksDB.length][0] = bookID;
+            copy[booksDB.length][1] = bookTitle;
+            copy[booksDB.length][2] = author;
+            copy[booksDB.length][3] = price;
+            copy[booksDB.length][4] = publisher;
+            booksDB = copy;
+        }
+
+    }
+
+
+    public static int getIndex(String bookID){
+        int index = -1;
+
+        for (int i =1; i < booksDB.length;i++){
+
+            if (booksDB[i][0].toLowerCase().equals(bookID.toLowerCase())) return i;
+        }
+
+        return index;
+    }
+
+    public static void printbooks() {
+
+        for (int i = 0; i < booksDB.length; i++) {
+            for (int j = 0; j < booksDB[0].length; j++)
+
+                System.out.println(booksDB[i][j]);
+
+
+        }
+        System.out.println("");
+    }
+
+
+    public static void writeBookInDB() {
+
+
+        List<String[]> replica = new ArrayList<String[]>();
+        try {
+            // https://mkyong.com/java/how-to-export-data-to-csv-file-java/
+            // create FileWriter object with file as parameter
+            FileWriter outputFile = new FileWriter(path);
+            // https://stackoverflow.com/questions/13969254/unwanted-double-quotes-in-generated-csv-file
+            // https://www.geeksforgeeks.org/writing-a-csv-file-in-java-using-opencsv/
+            // create CSVWriter object filewriter object as parameter
+            CSVWriter writer = new CSVWriter(outputFile, CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.DEFAULT_ESCAPE_CHARACTER, CSVWriter.RFC4180_LINE_END);
+
+
+            for (int i = 0; i < booksDB.length; i++) {
+
+                replica.add(booksDB[i]);
+            }
+
+            writer.writeAll(replica);
+            writer.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ app/src/test/java/de/buw/se4de/AppTest.java
@@ -0,0 +1,123 @@
+package de.buw.se4de;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.*;
+
+
+public class AppTest {
+	
+	login login;
+	home home;
+	NewBook nb;
+	NewSudent ns;
+	ActionEvent evt;
+		
+
+	
+	  @BeforeEach
+	  void setup() { 
+		  login = new login();
+		  nb = new NewBook(); 
+		  ns = new NewSudent();
+		  home = new home();
+	  }
+	  
+	  @Test
+	  void checkLogin() {
+		  try {
+			login.main(null);
+			login.initComponents();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		  login.userNameTextField.setText("admin");
+		  login.passwordTextField.setText("admin");
+		  login.loginBtnActionPerformed(evt);
+		  assertEquals(login.getLoginCheck(),true,"Login works if the credentials are same, login working as expected !");
+	  }
+	  
+	  @Test
+	  void checkClickFunctionalityWithStudent() {
+		  try {
+			ns.main(null);
+			ns.initComponents();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		  ns.studentIDTextBox.setText("87");
+	      ns.nameTextBox.setText("Anusha");
+	      ns.familynameTextBox.setText("oorugunda");
+		  ns.saveBtnActionPerformed(evt);
+		  assertEquals(ns.studentIDTextBox.getText(),!ns.studentIDTextBox.getText().isEmpty(),"The value should not be empty, as the form should be submitted !");
+	  }
+	  
+	  @Test
+	  void checkClickFunctionalityWithBook() {
+		  try {
+			nb.main(null);
+			nb.initComponents();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		  nb.bookIDTextField.setText("");
+	      nb.bookTitleTextField.setText("");
+	      nb.authorTextField.setText("");
+	      nb.priceTextField.setText("");
+	      nb.publishingYearTextField.setText("");
+	      DBConnection.setBook(nb.bookIDTextField.getText(), nb.bookTitleTextField.getText(), nb.authorTextField.getText(), nb.priceTextField.getText(), nb.publishingYearTextField.getText()); 
+	      
+	      Assertions.assertThrows(IOException.class, () ->  DBConnection.writeBookInDB(), "Expecting no exception from the outcome, if empty, the functionalty works !");
+      }
+	  
+	  @Test
+	  void checkDataRetrieval() {
+		  String[] columNames = {"Book ID", "Book Title", "Author Name", "Price", "Publishing Year"};
+		  Object[][] dataBase = DBConnection.booksDB;
+		  DefaultTableModel defTableModel = new DefaultTableModel(dataBase, columNames);
+		  int rowCount = defTableModel.getRowCount();
+		  boolean isGreater = rowCount > 0;
+		  assertEquals(isGreater,true ,"The value should be greater than zero, expecting retrieval properly !");
+	  }
+	  
+	  @Test
+	  void checkLogoutFunctionality() {
+		  try {
+			login.main(null);
+			login.initComponents();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		  login.userNameTextField.setText("admin");
+		  login.passwordTextField.setText("admin");
+		  login.loginBtnActionPerformed(evt);
+		  home.main(null);
+		  home.initComponents();
+		  home.logoutBtnActionPerformed(evt);
+		  boolean isUserActive = home.isActiveSession;
+		  assertEquals(isUserActive,false ,"The user should be able to logout properly !");
+	  }
+	  
+	 
+}
