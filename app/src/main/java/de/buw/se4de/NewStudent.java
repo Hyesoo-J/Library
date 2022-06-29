72910a4bf468dc79e68eb100e866e80913d2166d
NayeemZane <104457805+NayeemZane@users.noreply.github.com>
2022-06-29 15:40:37 +0200

    student fixed

--- app/src/main/java/de/buw/se4de/NewStudent.java
+++ app/src/main/java/de/buw/se4de/NewStudent.java
@@ -11,7 +11,7 @@ import java.awt.event.ActionListener;
 /**
  * @author nikhi
  */
-public class NewSudent extends javax.swing.JFrame {
+public class NewStudent extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public JButton saveBtn;
@@ -28,9 +28,9 @@ public class NewSudent extends javax.swing.JFrame {
     public JTextField familynameTextBox;
 
     /**
-     * Creates new form newSudent
+     * Creates new form NewStudent
      */
-    public NewSudent() {
+    public NewStudent() {
         initComponents();
     }
 
@@ -51,20 +51,20 @@ public class NewSudent extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewSudent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewStudent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewSudent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewStudent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewSudent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewStudent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewSudent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewStudent.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewSudent().setVisible(true);
+                new NewStudent().setVisible(true);
             }
         });
     }
